aa67c29d5ee972000c87b85641d72ccb8d8d1e98
reallylovesky <auliasyifanoerfazriah@gmail.com>
2024-01-25 08:45:20 +0700

    add migrations, model, seeder,controller

---
+++ database/migrations/2024_01_24_042407_create_vehicles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('guest_id')->constrained('guests')->cascadeOnDelete();
+            $table->enum('type', ['motor', 'car', 'bus']);
+            $table->string('license_plate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
